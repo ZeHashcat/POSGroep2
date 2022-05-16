742af3e313d9bfb71585ff642ee4eecd03a24596
ZeHashcat <89918012+ZeHashcat@users.noreply.github.com>
2022-05-16 15:17:36 +0200

    First Commit
    Setup complete.

---
+++ RPOS_Logic/OrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPoS_Model;
+using RPoS_DAL;
+
+namespace RPoS_Logic
+{
+    public class OrderService
+    {
+    }
+}
